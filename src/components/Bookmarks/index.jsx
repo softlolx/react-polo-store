5fc1df765e3b82cf7df4ce08fa674f1832ec6b65
softlolx <softlolx@gmail.com>
2022-08-25 14:23:40 +0300

    feat: goback button in bookmarks

--- src/components/Bookmarks/index.jsx
+++ src/components/Bookmarks/index.jsx
@@ -1,10 +1,17 @@
 import styles from "./Bookmarks.module.scss";
+import { useNavigate } from "react-router-dom";
 
 export function Bookmarks({ children }) {
+  const nav = useNavigate();
   return (
     <div className={styles.bookmarks}>
       <div className={styles.bookmarks__titleContainer}>
-        <button className={styles.bookmarks__backButton}></button>
+        <button
+          className={styles.bookmarks__backButton}
+          onClick={() => {
+            nav(-1);
+          }}
+        ></button>
         <h1 className={styles.bookmarks__title}>My wishlist</h1>
       </div>
       <div className={styles.bookmarks__cardsContainer}>{children}</div>
